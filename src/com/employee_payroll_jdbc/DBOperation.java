f14dd5a4bc27c44ca60861307ed26653c3ca8c2d
Dnyandeo bansode <bansodednyanadeo@gmail.com>
2022-05-23 23:15:39 +0530

    Added:-> Ability to find sum, average, min, max and number of male and female employees

--- src/com/employee_payroll_jdbc/DBOperation.java
+++ src/com/employee_payroll_jdbc/DBOperation.java
@@ -9,15 +9,25 @@ import java.sql.SQLException;
  * 
  * -->@Dnyandeo Bansode
  * */
+import java.util.Scanner;
 
 public class DBOperation {
 	DBConnection dbConnection = new DBConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Bansode@123");
+	Scanner sc = new Scanner(System.in);
+	
+	public void common(String query) throws ClassNotFoundException, SQLException {
+		Connection con = dbConnection.getConnection();
+		PreparedStatement psmt = con.prepareStatement(query);
+		psmt.execute();
+		dbConnection.closeConnections(con);
+		System.out.println("================================================================");
+	}
 	
 	//retrive data from database
-	public void retrive() throws SQLException, ClassNotFoundException {
+	public void retrive(String query) throws SQLException, ClassNotFoundException {
 		Connection con = dbConnection.getConnection();
 		//String query = "select * from employee_payroll where name = 'Sagar'";
-		String query = "select * from employee_payroll where start_date between cast('2019-01-01'\r\n" + "as date) and date(now());";
+		//String query = "select * from employee_payroll where start_date between cast('2019-01-01'\r\n" + "as date) and date(now());";
 		PreparedStatement psmt = con.prepareStatement(query);
 		ResultSet rs = psmt.executeQuery(query);
 		while (rs.next())
@@ -26,7 +36,8 @@ public class DBOperation {
 	        String firstName = rs.getString("name");
 	        String gender = rs.getString("gender");
 	        String salary = rs.getString("salary");
-	        String start_date = rs.getString("phone_no");
+	        String start_date = rs.getString("start_date");
+	        String phone_no = rs.getString("phone_no");
 	        String address = rs.getString("address");
 	        String departments = rs.getString("departments");
 	        String deduction = rs.getString("deduction");
@@ -34,19 +45,30 @@ public class DBOperation {
 	        String income_tax = rs.getString("income_tax");
 	        
 	        // print the results
-	        System.out.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", id, firstName, gender, salary, start_date, address, departments, deduction, taxable_pay, income_tax);
+	        System.out.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", id, firstName, gender, salary, start_date, phone_no, address, departments, deduction, taxable_pay, income_tax);
 	      }
 		System.out.println("records retrive successfully" );
 		dbConnection.closeConnections(con);
 	}
 	
-	public void updatetData() throws ClassNotFoundException, SQLException {
+	public void updatetData(String query) throws ClassNotFoundException, SQLException {
 		Connection con = dbConnection.getConnection();
-		String query = "update employee_payroll set salary = ? where id = 5";
+		//String query = "update employee_payroll set salary = ? where id = 3";
 		PreparedStatement psmt = con.prepareStatement(query);
-		psmt.setString(1, "3000000");
+		System.out.println("Enter salary:-> ");
+		int salary = sc.nextInt();
+		psmt.setLong(1, salary);
 		int i= psmt.executeUpdate();  
 		System.out.println(i+" records update successfully"); 
+		dbConnection.closeConnections(con);
+	}
+	
+	public void methodeOperation(String query) throws ClassNotFoundException, SQLException {
+		Connection con = dbConnection.getConnection();
+		PreparedStatement psmt = con.prepareStatement(query);
+		ResultSet rs = psmt.executeQuery(query);
+		rs.next();
+		System.out.println(rs.getString(1));
 		dbConnection.closeConnections(con);
 	}
 }
--- src/com/employee_payroll_jdbc/EmployeePayrollMains.java
+++ src/com/employee_payroll_jdbc/EmployeePayrollMains.java
@@ -1,6 +1,7 @@
 package com.employee_payroll_jdbc;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /*
  * ------------------------------Employee payroll Main ------------------------------
@@ -12,13 +13,34 @@ public class EmployeePayrollMains {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		//instance
-		DBOperation dbOperations = new DBOperation();
+		DBOperation dbOperations = new DBOperation(); 
+		Scanner sc = new Scanner(System.in);
 		
-		dbOperations.retrive();
-		System.out.println("------------------------------------------------------------");
-		
-		//update salary
-		dbOperations.updatetData();
-		System.out.println("------------------------------------------------------------");
+		while(true) {
+			System.out.println(" 1.Retive data \n 2.Update salary \n 3.Sum of salary");
+			System.out.print("Enter your choice:-> ");
+			int choice = sc.nextInt();
+			
+			switch(choice) {
+				case 1:
+					//String query = "select * from employee_payroll where name = 'Sagar'";
+					String query = "select * from employee_payroll where start_date between cast('2019-01-01'\r\n" + "as date) and date(now())";
+					dbOperations.retrive(query);
+					System.out.println("------------------------------------------------------------");
+					
+				case 2:
+					String query1 = "update employee_payroll set salary = ? where id = 2";
+					dbOperations.updatetData(query1);
+					System.out.println("------------------------------------------------------------");
+					
+				case 3:
+					//String query2 = "SELECT SUM(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender";
+					//String query2 = "SELECT avg(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender";
+					//String query2 = "SELECT min(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender";
+					//String query2 = "SELECT max(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender";
+					String query2 = "SELECT count(salary) FROM employee_payroll WHERE gender = 'M' GROUP BY gender";
+					dbOperations.methodeOperation(query2);
+			}
+		}
 	}
 }
